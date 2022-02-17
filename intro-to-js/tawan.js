fd7ace8ee2ec1c5b15e5f2c2d7d59e16508f92ab
Tawan Kheangamdee <enranime@gmail.com>
2022-02-17 14:02:56 +0700

    update

--- intro-to-js/tawan.js
+++ intro-to-js/tawan.js
@@ -13,3 +13,4 @@ function askMyName() {
  const h1message = document.getElementById("h1").innerHTML;// ใช้คำสั่ง document.getElementById เหมือนใน askMyName เพื่อเอาข้อความใน h1 มาใส่
     alert(h1message + 'ไม่ว่าพายุจะแรงแค่ไหน สุดท้ายก็ต้องสงบ ปัญหาที่เราประสบ ไม่นานก็จบและผ่านไป')
  }
+
